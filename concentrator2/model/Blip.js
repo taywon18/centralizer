4a3ff8cc2ba705597cba905f39fa14c1709afccf
taywon <thespartan18@hotmail.fr>
2020-06-14 18:58:56 +0200

    added color field

--- concentrator2/model/Blip.js
+++ concentrator2/model/Blip.js
@@ -2,12 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Blip = void 0;
 class Blip {
-    constructor(type, position, owner, text, persons) {
+    constructor(type, position, owner, text, persons, color = "blue") {
         this.type = type;
         this.position = position;
         this.owner = owner;
         this.text = text;
         this.persons = persons;
+        this.color = color;
     }
 }
 exports.Blip = Blip;
--- concentrator2/model/Blip.ts
+++ concentrator2/model/Blip.ts
@@ -4,16 +4,18 @@ import { Person } from "./Person"
 export class Blip {
 	type: string;
 	position: Position;
+	color: string;
 	owner: string;
 	text: string;
 	persons: Person[];
 
 
-	constructor(type: string, position: Position, owner: string, text: string, persons: Person[]) {
+	constructor(type: string, position: Position, owner: string, text: string, persons: Person[], color: string = "blue") {
 		this.type = type;
 		this.position = position;
 		this.owner = owner;
 		this.text = text;
 		this.persons = persons;
+		this.color = color;
 	}
 };
