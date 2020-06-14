f05b5f84aad35e3ebe0d9bd8836854a9045b38ad
taywon <thespartan18@hotmail.fr>
2020-06-14 16:41:09 +0200

    added more person data :D

--- concentrator2/model/Person.js
+++ concentrator2/model/Person.js
@@ -5,8 +5,13 @@ const Position_1 = require("./Position");
 class Person {
     constructor(data) {
         this.name = data["name"];
+        this.health = data["health"] | 0;
+        this.armor = data["armor"] | 0;
         this.occupation = data["occupation"];
         this.position = new Position_1.Position(data["position"]);
+        this.carType = data["carType"] | 0;
+        this.carHealth = data["carHealth"] | 0;
+        this.carPassengerCount = data["carPassengerCount"] | 0;
         this.lastRefresh = new Date();
     }
 }
--- concentrator2/model/Person.ts
+++ concentrator2/model/Person.ts
@@ -2,14 +2,26 @@ import { Position } from "./Position"
 
 export class Person {
 	name: string;
+	health: number;
+	armor: number;
+
 	occupation: string;
 	position: Position;
+	carType: number;
+	carHealth: number;
+	carPassengerCount: number;
+
 	lastRefresh: Date;
 
 	constructor(data: object) {
 		this.name = data["name"];
+		this.health = data["health"] | 0;
+		this.armor = data["armor"] | 0;
 		this.occupation = data["occupation"];
 		this.position = new Position(data["position"]);
+		this.carType = data["carType"] | 0;
+		this.carHealth = data["carHealth"] | 0;
+		this.carPassengerCount = data["carPassengerCount"] | 0;
 		this.lastRefresh = new Date();
 	}
 };
